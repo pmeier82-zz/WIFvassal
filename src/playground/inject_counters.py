148ae605823fd65e672448d4a065df0600e99204
Philipp Meier <pmeier82@googlemail.com>
2013-03-10 15:30:36 +0100

    * bf_base with cs 1-24

--- src/playground/inject_counters.py
+++ src/playground/inject_counters.py
@@ -3,34 +3,50 @@
 ##---IMPORTS
 
 from lxml import etree
-from ods_files import counter_sheet_row_idx_set, get_sheet, Unit, LandUnit
+from ods_files import counter_sheet_row_idx_set, get_sheet, Unit, LandUnit,\
+    AirUnit, NavalUnit
 from xml_parsing import GPIDGenerator, find_stack, PieceSlot
 
 ##---CONSTANTS
 
 indent = '                        '
-buildFile_base = '/home/pmeier/Workspace/WIFvassal/mod/buildFile_base.xml'
-buildFile_new = '/home/pmeier/Workspace/WIFvassal/mod/WiFdab/buildFile'
+BF_base = '/home/pmeier/Workspace/WIFvassal/mod/buildFile_base.xml'
+BF_new = '/home/pmeier/Workspace/WIFvassal/mod/WiFdab/buildFile'
 
 ##---FUNCTIONS
 
+def start(path=None):
+    global BF, GPID
+    with open(path or BF_base, 'r') as f:
+        BF = etree.parse(f)
+    GPID = GPIDGenerator(BF)
+
+
+def finish(path=None):
+    global BF
+    BF.write(path or BF_new, pretty_print=True)
+
+
 def get_control(u):
-    if u.power == 'China':
-        return {'Comm China': 'CC',
-                'Nat China': 'CN'}[u.home]
-    else:
-        return {'Commonwealth': 'CW',
-                'France': 'FR',
-                'Germany': 'GE',
-                'Italy': 'IT',
-                'Japan': 'JP',
-                'USSR': 'RU',
-                'USA': 'US'}[u.power]
+    try:
+        if u.power == 'China':
+            return {'Comm China': 'CC',
+                    'Nat China': 'CN'}[u.home]
+        else:
+            return {'Commonwealth': 'CW',
+                    'France': 'FR',
+                    'Germany': 'GE',
+                    'Italy': 'IT',
+                    'Japan': 'JP',
+                    'USSR': 'RU',
+                    'USA': 'US'}[u.power]
+    except:
+        return 'Minor'
 
 
 def get_type(u):
     rval = u.type
-    if 'Div' in u.size:
+    if 'Div' in getattr(u, 'size', ''):
         if u.clas == 'ART':
             if u.type not in ['AT', 'AA', 'AAA']:
                 rval = rval[0] + rval[1:].lower()
@@ -43,49 +59,154 @@ def get_type(u):
                     rval = 'SP ' + rval
         else:
             rval = rval[0] + rval[1:].lower()
-    if u.type in ['CAV', 'FTR', 'LND', 'NAV', 'ATR', 'CVP', 'SYNTH']:
+    if u.type in ['CAV', 'SYNTH', 'ATR', 'CVP', 'FTR', 'LND', 'NAV']:
         rval += '-{:d}'.format(u.cost)
+    if u.type in ['ASW', 'CV', 'CVL']:
+        rval += '-{:d}'.format(u.time)
     return rval
 
 
 def inject_land(cs_no):
-    """land units"""
+    # init
+    global BF, GPID
+    stack = find_stack(BF, cs_no, 'LND')
+    sheet = get_sheet('Land')
+    header = Unit.read_header(sheet)
+    cs_rids = counter_sheet_row_idx_set(sheet, cs_no)
+    assert stack is not None and\
+           sheet is not None and\
+           header is not None and\
+           cs_rids is not None, 'setup error cs_no: %d' % cs_no
 
-    # init build file
-    bf = None
-    with open(buildFile_base, 'r') as f:
-        bf = etree.parse(f)
-    gpid = GPIDGenerator(bf)
-    stack = find_stack(bf, cs_no, 'LND')
-
-    # init ods file
-    LAND = get_sheet('Land')
-    header = Unit.read_header(LAND)
-    cs_rids = counter_sheet_row_idx_set(LAND, cs_no)
+    # produce counters and piece slots
     for rid in cs_rids:
         # load unit
-        lu = LandUnit()
-        lu.update(LAND, rid, header)
+        u = LandUnit()
+        u.update(sheet, rid, header)
 
         # piece slot
-        img_name = 'cs{:02d}_{:02d}_{:02d}.png'.format(
-            cs_no, lu.row, lu.col)
-        ps = PieceSlot(lu.unit, gpid.get(), img_name)
-        ps.add_trait('mark', ('kit', lu.kit))
-        ps.add_trait('mark', ('cs_col', lu.col))
-        ps.add_trait('mark', ('cs_row', lu.row))
-        ps.add_trait('mark', ('cs_no', lu.cs))
-        ps.add_trait('mark', ('MOV', lu.mov))
-        if lu.rog:
-            ps.add_trait('mark', ('ROG', lu.rog))
-        ps.add_trait('mark', ('STR', lu.str))
-        ps.add_trait('prototype', (get_type(lu), ''))
-        ps.add_trait('prototype', ('Control' + get_control(lu), ''))
+        img = 'cs{:02d}_{:02d}_{:02d}.png'.format(
+            cs_no, u.row, u.col)
+        ps = PieceSlot(u.name, GPID.get(), img)
+        ps.add_trait('mark', ('kit', u.kit))
+        ps.add_trait('mark', ('cs_col', u.col))
+        ps.add_trait('mark', ('cs_row', u.row))
+        ps.add_trait('mark', ('cs_no', u.cs))
+        ps.add_trait('mark', ('YEAR', u.year))
+        ps.add_trait('mark', ('MOV', u.mov))
+        if u.rog:
+            ps.add_trait('mark', ('ROG', u.rog))
+        ps.add_trait('mark', ('STR', u.str))
+        ps.add_trait('prototype', (get_type(u), ''))
+        ps.add_trait('prototype', ('Control' + get_control(u), ''))
         stack.append(ps.get_xml())
 
-    bf.write(buildFile_new, pretty_print=True)
+
+def inject_air(cs_no):
+    # init
+    global BF, GPID
+    stack = find_stack(BF, cs_no, 'AIR')
+    sheet = get_sheet('Air')
+    header = Unit.read_header(sheet)
+    cs_rids = counter_sheet_row_idx_set(sheet, cs_no)
+    assert stack is not None and\
+           sheet is not None and\
+           header is not None and\
+           cs_rids is not None, 'setup error cs_no: %d' % cs_no
+
+    # produce counters and piece slots
+    for rid in cs_rids:
+        # load unit
+        u = AirUnit()
+        u.update(sheet, rid, header)
+
+        # piece slot
+        img = 'cs{:02d}_{:02d}_{:02d}.png'.format(
+            cs_no, u.row, u.col)
+        ps = PieceSlot(' '.join([u.name, u.name2]).strip(), GPID.get(), img)
+        ps.add_trait('mark', ('kit', u.kit))
+        ps.add_trait('mark', ('cs_col', u.col))
+        ps.add_trait('mark', ('cs_row', u.row))
+        ps.add_trait('mark', ('cs_no', u.cs))
+        ps.add_trait('mark', ('YEAR', u.year))
+        if u.cvp_y1 is not None:
+            ps.add_trait('mark', ('CVP_Y1', u.cvp_y1))
+        if u.cvp_y1 is not None:
+            ps.add_trait('mark', ('CVP_Y2', u.cvp_y2))
+        if u.cvp_y1 is not None:
+            ps.add_trait('mark', ('CVP_Y3', u.cvp_y3))
+        if u.cvp_s1 is not None:
+            ps.add_trait('mark', ('CVP_S1', u.cvp_s1))
+        if u.cvp_s2 is not None:
+            ps.add_trait('mark', ('CVP_S2', u.cvp_s2))
+        if u.cvp_s3 is not None:
+            ps.add_trait('mark', ('CVP_S3', u.cvp_s3))
+        if u.str is not None:
+            ps.add_trait('mark', ('STR', u.str))
+        if u.str is not None:
+            ps.add_trait('mark', ('STR', u.str))
+        if u.tac is not None:
+            ps.add_trait('mark', ('TAC', u.tac))
+        if u.ats is not None:
+            ps.add_trait('mark', ('ATS', u.ats))
+        if u.ata is not None:
+            ps.add_trait('mark', ('ATA', u.ata))
+        ps.add_trait('prototype', (get_type(u), ''))
+        ps.add_trait('prototype', ('Control' + get_control(u), ''))
+        stack.append(ps.get_xml())
+
+
+def inject_sea(cs_no):
+    # init
+    global BF, GPID
+    stack = find_stack(BF, cs_no, 'SEA')
+    sheet = get_sheet('Naval')
+    header = Unit.read_header(sheet)
+    cs_rids = counter_sheet_row_idx_set(sheet, cs_no)
+    assert stack is not None and\
+           sheet is not None and\
+           header is not None and\
+           cs_rids is not None, 'setup error cs_no: %d' % cs_no
+
+    # produce counters and piece slots
+    for rid in cs_rids:
+        # load unit
+        u = NavalUnit()
+        u.update(sheet, rid, header)
+        if u.type == 'CONV':
+            continue
+
+        # piece slot
+        img = 'cs{:02d}_{:02d}_{:02d}.png'.format(
+            cs_no, u.row, u.col)
+        ps = PieceSlot(' '.join([u.name, u.name2]).strip(), GPID.get(), img)
+        ps.add_trait('mark', ('kit', u.kit))
+        ps.add_trait('mark', ('cs_col', u.col))
+        ps.add_trait('mark', ('cs_row', u.row))
+        ps.add_trait('mark', ('cs_no', u.cs))
+        ps.add_trait('mark', ('YEAR', u.year))
+        ps.add_trait('mark', ('RNG', u.rng))
+        ps.add_trait('mark', ('MOV', u.mov))
+        if u.cv is not None:
+            ps.add_trait('mark', ('CV', u.cv))
+        if u.sb is not None:
+            ps.add_trait('mark', ('SB', u.sb))
+        ps.add_trait('mark', ('AA', u.aa))
+        ps.add_trait('mark', ('DEF', u.dfs))
+        ps.add_trait('mark', ('ATT', u.att))
+        ps.add_trait('prototype', (get_type(u), ''))
+        ps.add_trait('prototype', ('Control' + get_control(u), ''))
+        stack.append(ps.get_xml())
 
 ##---MAIN
 
 if __name__ == '__main__':
-    inject_land(1)
+    # init
+    CS = 1
+
+    # process
+    start()
+    inject_land(CS)
+    inject_air(CS)
+    inject_sea(CS)
+    finish()
--- src/playground/ods_files.py
+++ src/playground/ods_files.py
@@ -32,13 +32,14 @@ class Unit(object):
     def __init__(self):
         self.sh_name = None
         self.sh_row = None
-        self.loc = None
         self.side = None
         self.power = None
         self.home = None
         self.clas = None
         self.type = None
+        self.name = None
         self.year = None
+        self.cost = None
         self.time = None
         self.kit = None
         self.cs = None
@@ -66,18 +67,20 @@ class Unit(object):
         # set values
         self.sh_name = sheet.name
         self.sh_row = row_no
-        self.side = str(row[header['SIDE']].plaintext())
-        self.power = str(row[header['POWER']].plaintext())
-        self.home = str(row[header['HOME']].plaintext())
-        self.clas = str(row[header['CLASS']].plaintext())
-        self.type = str(row[header['TYPE']].plaintext())
-        self.year = str(row[header['YEAR']].plaintext())
-        self.time = int(row[header['TIME']].plaintext())
-        self.kit = str(row[header['KIT']].plaintext())
-        self.cs = int(row[header['CS']].value)
-        self.row = int(row[header['ROW']].value)
-        self.col = int(row[header['COL']].value)
-        self.option = str(row[header['OPTION']].plaintext())
+        self.side = self.xml_str(row[header['SIDE']].plaintext())
+        self.power = self.xml_str(row[header['POWER']].plaintext())
+        self.home = self.xml_str(row[header['HOME']].plaintext())
+        self.clas = self.xml_str(row[header['CLASS']].plaintext())
+        self.type = self.xml_str(row[header['TYPE']].plaintext())
+        self.name = self.xml_str(row[header['NAME']].plaintext())
+        self.year = self.xml_str(row[header['YEAR']].plaintext())
+        self.cost = self.xml_int(row[header['COST']].plaintext())
+        self.time = self.xml_int(row[header['TIME']].plaintext())
+        self.kit = self.xml_str(row[header['KIT']].plaintext())
+        self.cs = self.xml_int(row[header['CS']].value)
+        self.row = self.xml_int(row[header['ROW']].value)
+        self.col = self.xml_int(row[header['COL']].value)
+        self.option = self.xml_str(row[header['OPTION']].plaintext())
 
         return header, row
 
@@ -85,22 +88,36 @@ class Unit(object):
         return '{sh_name}#{sh_row} {}'.format(self.__dict__, **self.__dict__)
 
     def __cmp__(self, other):
-        if self.cs != other.u_cs:
-            return cmp(self.cs, other.u_cs)
+        if self.cs != other.cs:
+            return cmp(self.cs, other.cs)
         else:
-            if self.row != other.u_row:
-                return cmp(self.row, other.u_row)
+            if self.row != other.row:
+                return cmp(self.row, other.row)
             else:
-                return cmp(self.col, other.u_col)
+                return cmp(self.col, other.col)
+
+    @classmethod
+    def xml_str(cls, v):
+        try:
+            return v.strip()
+        except:
+            return 'ERROR'
+
+    @classmethod
+    def xml_int(cls, v):
+        try:
+            return int(v)
+        except:
+            try:
+                return int(v[1:-1])
+            except:
+                return None
 
 
 class LandUnit(Unit):
-    """land unit"""
-
     def __init__(self):
         super(LandUnit, self).__init__()
 
-        self.unit = None
         self.str = None
         self.rog = None
         self.mov = None
@@ -114,52 +131,124 @@ class LandUnit(Unit):
         self.patif = None
         self.used_pa = None
         self.aif_patif = None
-        self.col_f = None
-        self.col_b = None
 
     def update(self, sheet, row_no, header=None):
         # super
         header, row = super(LandUnit, self).update(sheet, row_no, header)
 
         # set values
-        self.unit = str(row[header['UNIT']].plaintext())
-        self.str = int(row[header['STR']].value)
+        self.str = self.xml_int(row[header['STR']].value)
         if row[header['ROG']].value:
-            self.rog = int(row[header['ROG']].value)
-        self.mov = int(row[header['MOV']].value)
-        self.cost = int(row[header['COST']].value)
-        self.size = str(row[header['SIZE']].plaintext())
-        self.other = str(row[header['OTHER']].plaintext())
-        self.abilities = str(row[header['ABILITIES']].plaintext())
-        self.used_a = str(row[header['USED A']].plaintext())
-        self.aif = str(row[header['AIF']].plaintext())
-        self.used_p = str(row[header['USED P']].plaintext())
-        self.patif = str(row[header['PatiF']].plaintext())
-        self.used_pa = str(row[header['USED PA']].plaintext())
-        self.aif_patif = str(row[header['AiF + PatiF']].plaintext())
-        self.col_f = str(row[header['FORE COLOR']].plaintext())
-        self.col_b = str(row[header['BACK COLOR']].plaintext())
+            self.rog = self.xml_int(row[header['ROG']].value)
+        self.mov = self.xml_int(row[header['MOV']].value)
+        self.size = self.xml_str(row[header['SIZE']].plaintext())
+        self.other = self.xml_str(row[header['OTHER']].plaintext())
+        self.abilities = self.xml_str(row[header['ABILITIES']].plaintext())
+        self.used_a = self.xml_str(row[header['USED A']].plaintext())
+        self.aif = self.xml_str(row[header['AIF']].plaintext())
+        self.used_p = self.xml_str(row[header['USED P']].plaintext())
+        self.patif = self.xml_str(row[header['PatiF']].plaintext())
+        self.used_pa = self.xml_str(row[header['USED PA']].plaintext())
+        self.aif_patif = self.xml_str(row[header['AiF + PatiF']].plaintext())
+
+
+class AirUnit(Unit):
+    def __init__(self):
+        super(AirUnit, self).__init__()
+
+        self.name2 = None
+        self.ata = None
+        self.ats = None
+        self.tac = None
+        self.str = None
+        self.rng = None
+        self.other = None
+        self.used = None
+        self.cvp_y1 = None
+        self.cvp_s1 = None
+        self.cvp_y2 = None
+        self.cvp_s2 = None
+        self.cvp_y3 = None
+        self.cvp_s3 = None
+
+    def update(self, sheet, row_no, header=None):
+        # super
+        header, row = super(AirUnit, self).update(sheet, row_no, header)
+
+        # set values
+        self.name2 = self.xml_str(row[header['NAME2']].plaintext())
+        self.ata = self.xml_int(row[header['ATA']].plaintext())
+        self.ats = self.xml_int(row[header['ATS']].plaintext())
+        self.tac = self.xml_int(row[header['TAC']].plaintext())
+        self.str = self.xml_int(row[header['STR']].plaintext())
+        self.rng = self.xml_int(row[header['RANGE']].value)
+        self.other = self.xml_str(row[header['OTHER']].plaintext())
+        self.used = self.xml_str(row[header['USED']].plaintext())
+        self.cvp_y1 = self.xml_int(row[header['YR1']].plaintext())
+        self.cvp_s1 = self.xml_int(row[header['SIZ1']].plaintext())
+        self.cvp_y2 = self.xml_int(row[header['YR2']].plaintext())
+        self.cvp_s2 = self.xml_int(row[header['SIZ2']].plaintext())
+        self.cvp_y3 = self.xml_int(row[header['YR3']].plaintext())
+        self.cvp_s3 = self.xml_int(row[header['SIZ3']].plaintext())
+
+
+class NavalUnit(Unit):
+    def __init__(self):
+        super(NavalUnit, self).__init__()
+
+        self.name2 = None
+        self.att = None
+        self.dfs = None
+        self.aa = None
+        self.sb = None
+        self.rng = None
+        self.mov = None
+        self.cv = None
+        self.cost2 = None
+        self.sunk = None
+        self.used = None
+
+    def update(self, sheet, row_no, header=None):
+        # super
+        header, row = super(NavalUnit, self).update(sheet, row_no, header)
+
+        # set values
+        self.name2 = self.xml_str(row[header['NAME2']].plaintext())
+        self.att = self.xml_int(row[header['ATT']].plaintext())
+        self.dfs = self.xml_int(row[header['DEF']].plaintext())
+        self.aa = self.xml_int(row[header['AA']].plaintext())
+        self.sb = self.xml_int(row[header['SB']].plaintext())
+        self.rng = self.xml_int(row[header['RNG']].value)
+        self.mov = self.xml_int(row[header['MOV']].plaintext())
+        self.cv = self.xml_int(row[header['CV']].plaintext())
+        self.sunk = self.xml_str(row[header['SUNK']].plaintext())
+        self.used = self.xml_str(row[header['USED']].plaintext())
 
 ##---MAIN
 
 if __name__ == '__main__':
-    LAND = get_sheet('Land')
-    header = Unit.read_header(LAND)
-    cs1rid = counter_sheet_row_idx_set(LAND, 3)
-    CS1 = []
-    for rid in cs1rid:
-        lu = LandUnit()
-        lu.update(LAND, rid, header)
-        CS1.append(lu)
+    MODE = 'Naval'
+    CS = 1
+    SH = get_sheet(MODE)
+    header = Unit.read_header(SH)
+    cs_rids = counter_sheet_row_idx_set(SH, CS)
+    CS = []
+    for rid in cs_rids:
+        u = {'Land': LandUnit,
+             'Air': AirUnit,
+             'Naval': NavalUnit}[MODE]()
+        u.update(SH, rid, header)
+        CS.append(u)
 
     def pp_cs(cs):
         for u in cs:
-            print '({r:02d}-{c:02d}) {name}'.format(r=u.u_row,
-                                                    c=u.u_col,
-                                                    name=u.lu_unit)
+            name = ' '.join([u.name, u.name2]).strip()
+            print '({cs:02d}:{row:02d}-{col:02d}) [{type}] {name}'.format(
+                cs=u.cs, row=u.row, col=u.col, type=u.type, name=name),
+            print '[CV: {}]'.format(u.cv)
 
-    pp_cs(CS1)
+    pp_cs(CS)
     print '#' * 20
 
-    CS1.sort()
-    pp_cs(CS1)
+    CS.sort()
+    pp_cs(CS)
